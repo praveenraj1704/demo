db92180d68d1e3a156cdaaa4e62397152199e89a
praveenraj1704 <praveenraj1704@gmail.com>
2022-12-30 22:53:54 +0530

    Done

---
+++ src/org/demoloop/Looping.java
@@ -0,0 +1,19 @@
+package org.demoloop;
+
+
+public class Looping {
+
+	public static void main(String[] args) {
+		int sum = 1 ;
+		for (int i = 1; i <=5; ++i) {
+				
+					sum*=i;
+					
+			}
+		System.out.println(sum);
+			}
+	
+		}
+
+	
+	
